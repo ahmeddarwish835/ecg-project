170a3a3d8268426ee5e9fb7db9d7e4c7be4529c5
ahmeddarwish835 <ahmeddarwishh24@gmail.com>
2026-05-16 19:38:05 +0300

    fix api url

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://127.0.0.1:8020';
+const API_URL = 'https://ecg-project-production.up.railway.app';
 
 export async function analyzeEcg(testId, filterType) {
     const res = await fetch(`${API_URL}/analyze`, {
